8448fdca53c116a58de3d7ec16b01ad5372dc824
eugeny naumov <min.verkligheten@gmail.com>
2015-07-27 12:46:49 +0300

    Add account activations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,6 +10,10 @@ class UserTest < ActiveSupport::TestCase
     assert @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end
+
   test "name should be present" do
     @user.name = "     "
     assert_not @user.valid?
